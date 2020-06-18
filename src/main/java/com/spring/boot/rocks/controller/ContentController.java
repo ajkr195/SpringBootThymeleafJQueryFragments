68d5eba175714ab108f15db12793d2328ede8038
ajay <ajkr195@gmail.com>
2020-06-18 11:32:27 -0400

    update

--- src/main/java/com/spring/boot/rocks/controller/ContentController.java
+++ src/main/java/com/spring/boot/rocks/controller/ContentController.java
@@ -1,4 +1,4 @@
-package com.spring.boot.rocks;
+package com.spring.boot.rocks.controller;
 
 import java.util.List;
 
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.boot.rocks.model.AppUser;
+import com.spring.boot.rocks.repository.AppUserRepository;
 
 @Controller
 @RequestMapping("/")
@@ -22,33 +25,33 @@ public class ContentController {
 
     @RequestMapping("/content1")
     public String getContent1() {
-        return "content :: content1";
+        return "content/mycontent :: content1";
     }
 
     @RequestMapping("/content2")
     public String getContent2() {
-        return "content :: content2";
+        return "content/mycontent :: content2";
     }
     
     @RequestMapping("/active")
     public String getactiveUsers(Model model) {
     	List<AppUser> users = appUserRepository.findByUserenabledTrue();
 		model.addAttribute("users", users);
-        return "users :: resultsList";
+        return "tables/usertable :: resultsList";
     }
     
     @RequestMapping("/inactive")
     public String getinactiveUsers(Model model) {
     	List<AppUser> users = appUserRepository.findByUserenabledFalse();
 		model.addAttribute("users", users);
-        return "users :: resultsList";
+        return "tables/usertable :: resultsList";
     }
     
     @RequestMapping("/all")
     public String getUsers(Model model) {
     	List<AppUser> users = appUserRepository.findAll();
 		model.addAttribute("users", users);
-        return "users :: resultsList";
+        return "tables/usertable :: resultsList";
     }
     
     @RequestMapping("/users")
--- src/main/java/com/spring/boot/rocks/repository/AppUserRepository.java
+++ src/main/java/com/spring/boot/rocks/repository/AppUserRepository.java
@@ -1,9 +1,11 @@
-package com.spring.boot.rocks;
+package com.spring.boot.rocks.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.spring.boot.rocks.model.AppUser;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
